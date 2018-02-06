2a4075f18d63e70bdb385da97d63a95768a6e543
Darian Lio <14dl50@queensu.ca>
2018-02-05 20:06:44 -0500

    method overloading review

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -1,11 +1,50 @@
 public class MethodOverloading {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int newScore = calculateScore("Darian", 500);
         System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+        double centimeters = calcFeetAndInchesToCentimeters(6,0);
+        if(centimeters < 0.0){
+            System.out.println("Invalid parameters");
+        }
+        calcFeetAndInchesToCentimeters(157);
     }
 
-    public static int calculateScore(String playerName, int score){
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if (feet < 0 || (inches < 0 || inches > 12)){
+            return -1;
+        }
+
+        double cm = feet*12*2.54 + inches*2.54;
+        return cm;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if (inches < 0){
+            return -1;
+        }
+
+        double feet = (int) inches/12;
+        double remainingInches = (int) inches % 12;
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+
+
+    public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points");
-        return score*1000;
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("No player name, no player score.");
+        return 0;
     }
 }
+
+
